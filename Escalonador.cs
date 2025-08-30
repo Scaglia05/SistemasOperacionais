fd270971ffb54b46f486cf2efdbae6460b579605
Guilherme Scaglia <guiscaglia2011@hotmail.com>
2025-08-30 17:31:32 -0300

    Adicionar arquivos de projeto.

---
+++ Escalonador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasOperacionais
+{
+    public class Escalonador
+    {
+        public List<Processo> Pronto { get; set; }
+        public List<Processo> Executando { get; set; }
+        public List<Processo> Bloqueados { get; set; }
+
+        public void AddProcesso() { }
+        public void ProximoFifo() { }
+
+        public void ProximoRoundRobin() { }
+        public void ProximoSJF() { }
+    }
+}

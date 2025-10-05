45cc424358f1ba15c1dcb5053f534f819e58d6c9
Guilherme Scaglia <guiscaglia2011@hotmail.com>
2025-10-05 17:55:51 -0300

    .

--- obj/Debug/net9.0/SistemasOperacionais.AssemblyInfo.cs
+++ obj/Debug/net9.0/SistemasOperacionais.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemasOperacionais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad601a9e9816c5bcfd16650c929953f7a266a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e344b84326dd9b6a8bbe186628ea987a681a3d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemasOperacionais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemasOperacionais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
